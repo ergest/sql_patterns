905a7f261d1a1dddbb8c8248b8857f0e215be8b0
Ergest Xheblati <ergest@gmail.com>
2022-03-25 19:09:57 -0400

    updates

--- Book/build.sh
+++ Book/build.sh
@@ -1,1 +1,3 @@
-pandoc chap*.md -o output/book.pdf --from markdown --toc --template eisvogel --listings
+pandoc chap*.md -o output/book.pdf --from markdown --toc --indented-code-classes=sql --highlight-style=monochrome -V mainfont="Palatino" -V documentclass=report -V papersize=A5 -V geometry:margin=1in
+
+  #--template eisvogel --listings
