a223ccfcd62fbf684dee079a552b870026bb83f6
Ergest Xheblati <ergest@gmail.com>
2024-06-23 13:58:14 -0400

    ficed case

--- chapter_code_listings/chapter4/listing_4_13.sql
+++ chapter_code_listings/chapter4/listing_4_13.sql
@@ -2,7 +2,7 @@
 SELECT
     q.id AS post_id,
     q.creation_date,
-    date_part('week', creation_date) as week_of_year
+    DATE_PART('week', creation_date) as week_of_year
 FROM
     posts_questions q
 WHERE
